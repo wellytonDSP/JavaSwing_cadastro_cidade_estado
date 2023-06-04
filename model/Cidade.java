c38a89d6774853aaa5ffb1aa02352d9c0c48500a
wellytonDSP <wellyton.santos@outlook.com>
2023-06-04 16:02:08 -0300

    Telas Swing

---
+++ model/Cidade.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.pr.senaccadastros.model;
+
+/**
+ *
+ * @author welly
+ */
+public class Cidade {
+    
+    private String nome;
+    private Estado estado;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the estado
+     */
+    public Estado getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    
+}
---
+++ model/Estado.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.pr.senaccadastros.model;
+
+/**
+ *
+ * @author welly
+ */
+public class Estado {
+    
+    private String nomeEstado;
+    private String siglaEstado;
+
+    /**
+     * @return the nomeEstado
+     */
+    public String getNomeEstado() {
+        return nomeEstado;
+    }
+
+    /**
+     * @param nomeEstado the nomeEstado to set
+     */
+    public void setNomeEstado(String nomeEstado) {
+        this.nomeEstado = nomeEstado;
+    }
+
+    /**
+     * @return the siglaEstado
+     */
+    public String getSiglaEstado() {
+        return siglaEstado;
+    }
+
+    /**
+     * @param siglaEstado the siglaEstado to set
+     */
+    public void setSiglaEstado(String siglaEstado) {
+        this.siglaEstado = siglaEstado;
+    }
+    
+    
+    
+}
